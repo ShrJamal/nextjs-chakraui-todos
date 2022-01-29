bd2aab94ed290dca775e5fc1440a36a95720c73e
Jamal .S <me@shrjamal.com>
2022-01-29 23:58:56 +0100

    add todos api

---
+++ src/pages/api/todos/index.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const prisma = new PrismaClient()
+  if (req.method === 'GET') {
+    const todos = await prisma.todo.findMany()
+    res.status(200).json(todos)
+  } else if (req.method === 'POST') {
+    const content = JSON.parse(req.body)['content'] as string
+    if (!content) {
+      res.status(400).end('Content is required')
+      return
+    }
+
+    const todo = await prisma.todo
+      .create({
+        data: {
+          content,
+        },
+      })
+      .catch((e) => res.status(400).end(e.message))
+    res.status(200).json(todo)
+  }
+}
